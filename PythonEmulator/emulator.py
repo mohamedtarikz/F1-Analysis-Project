2bf30f6739dc7da8067a2f30276a5e37febae0b0
mohamedtarikz <mohamedtarik06@gmail.com>
2026-06-30 05:15:43 +0300

    edit port number

--- PythonEmulator/emulator.py
+++ PythonEmulator/emulator.py
@@ -5,14 +5,14 @@ import json
 
 # 1. fetching telemetry from server
 print("Fetching data from OpenF1...")
-url = "https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159"
+url = "https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=1"
 response = requests.get(url)
 telemetry = response.json()
 print(f"Loaded {len(telemetry)} telemetry records")
 
 # 2. set up a local socket to stream to our Java app
 JAVA_SERVER_IP = "localhost"
-JAVA_SERVER_PORT = 8080
+JAVA_SERVER_PORT = 9999
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(f"Connecting to {JAVA_SERVER_IP}:{JAVA_SERVER_PORT}...")
